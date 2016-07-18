d81024fb02b6baeccd8957c7da1c10396f34c895
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-07-18 08:50:19 -0700

    Init, Preappend, and head methods done

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,21 +1,23 @@
 class LinkedList
-	attr_accessor :value, :next_node
-	def initialize(node = nil)
-		@node = node
+	attr_accessor :value, :next_node, :node
+	def initialize(node)
+		@head = node
 		#this will not just add one node but it will add the one node and all nodes that the node points to
 	end
 
 	def append(node)#adds value to the end of the list
-		
 	end
 
 	def preappend(node)#adds value to the begining of the list
+		node.next_node = @head
+		@head = node
 	end
 
 	def size(node)#not node but cant think of what to put here
 	end
 
-	def head(node)#returns the first value of the list
+	def head#returns the first value of the list
+		@head
 	end
 
 	def tail(node)#returns the last value of the list
@@ -35,9 +37,9 @@ class LinkedList
 
 	def to_s(msg = nil)#represents your linked list objects as strings so that you can preview them in the console
 		msg ||= ""
-		return msg[0..-4]if self.value.nil?
-		msg << "#{self.value} ->"
-		self.next_node.to_s
+		return msg[0..-4]if self.node.value.nil?
+		msg << "#{self.node.value} ->"
+		self.node.next_node.to_s
 	end
 
 end
@@ -49,22 +51,16 @@ class Node
 		@value = value
 		@next_node = next_node
 	end
-
-	def self.node_list(node, msg = nil)
-		msg ||= ""
-		return msg[0..-4]if node.nil?
-		node_list(node.next_node, msg << "#{node.value} ->")
-	end
 end
 node_app = Node.new("Appended")
 
 node_5 = Node.new("5")
-node_4 = Node.new("4", node_5)
-node_3 = Node.new("3", node_4)
-node_2 = Node.new("2", node_3)
-node_1 = Node.new("1", node_2)
+node_4 = Node.new("4")
+node_3 = Node.new("3")
+node_2 = Node.new("2")
+node_1 = Node.new("1")
 
 list_1 = LinkedList.new(node_1)
-
 puts list_1.inspect
+list_1.append(node_2)
 
